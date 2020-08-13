0312a7c758ec35f031df4770d987ebff58b213d7
sidou <sidahmed.issolah@gmail.com>
2020-08-13 20:07:35 +0100

    modification in the front end

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 async function dialog() {
     var pathcsv = "";
-    const {dialog} = require('electron').remote ;
+    const { dialog } = require('electron').remote;
     await dialog.showOpenDialog({ properties: ['openDirectory'] }).then(result => {
         pathcsv += result.filePaths[0];
 
@@ -10,11 +10,13 @@ async function dialog() {
     return pathcsv;
 }
 document.querySelector('#title').style.display = "none";
-var exestingEventValue = false;
+var exestingEventValue = true;
+var i;
 
 function newEvent() {
     document.querySelector('#event').style.display = "none";
     document.querySelector('#title').style.display = "block";
+    exestingEventValue = false
 
 }
 
@@ -32,7 +34,7 @@ async function exestingEvent() {
     document.querySelector('#form_input').style.display = "block";
 
     titleP = pathcsv.split('.')[0]
-    exestingEventValue = true
+
 }
 
 var titleP;
@@ -52,7 +54,6 @@ async function newTitle() {
         title.style.display = "none";
         document.querySelector('#sendTitle').style.display = "none";
         document.querySelector('#form_input').style.display = "block";
-
         return title;
     }
 }
@@ -67,9 +68,9 @@ function dataForm() {
     var phoneNumber = document.querySelector('#phoneNumber').value;
     document.querySelector('#form_input').style.display = "block";
 
-    if(name == '' || lastName == '' || mail== '' || phoneNumber =='' )
+    if (name == '' || lastName == '' || mail == '' || phoneNumber == '')
         return emptyFiled;
-    else{
+    else {
         var data = [{
             name: name,
             lastName: lastName,
@@ -85,7 +86,8 @@ function dataForm() {
 
 
 
-async function csv(exestingEventValue,title, data) {
+async function csv(exestingEventValue, title, data) {
+
     const createCsvWriter = require('csv-writer').createObjectCsvWriter;
     const csvWriter = createCsvWriter({
         path: title + '.csv',
@@ -97,7 +99,6 @@ async function csv(exestingEventValue,title, data) {
             { id: 'phoneNumber', title: 'Phone Number' }
         ]
     });
-    exestingEventValue = false
     let writed = false;
     await csvWriter.writeRecords(data).then(() => {
         writed = true;
@@ -120,7 +121,7 @@ function myFunctions(t) {
     } else {
         $('#badInfos').css('display', 'none');
         $('#goodInfos').css('display', 'none');
-        csv(exestingEventValue,titleP, datas).then(() => {
+        csv(exestingEventValue, titleP, datas).then(() => {
             $('#goodInfos').html('<h3>save successfully</h3>');
             $('#goodInfos').css('display', 'block');
             $('#myModal').modal('show');
@@ -141,15 +142,22 @@ $('#goodInfos').css('display', 'none');
 
 function toExcel() {
     const convertCsvToXlsx = require('@aternus/csv-to-xlsx');
- 
-    let source = titleP+'.csv';
-    let destination = titleP+'.xlsx';
-     
+
+    let source = titleP + '.csv';
+    let destination = titleP + '.xlsx';
+
     try {
-      convertCsvToXlsx(source, destination,true);
+        convertCsvToXlsx(source, destination, true);
+        $('#goodInfos').html('<h3>exported successfully</h3>');
+        $('#goodInfos').css('display', 'block');
+        $('#myModal').modal('show');
     } catch (e) {
-      console.error(e.toString());
+        console.error(e.toString());
+        $('#badInfos').html('<h3>export failed</h3>');
+        $('#badInfos').css('display', 'block');
+        $('#myModal').modal('show');
     }
 
     // 
 }
+s
